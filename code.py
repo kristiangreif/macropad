2b67a8973d1e9384a0da3c875a98a9df62c6648f
Kristián Grief <62846329+kristiangreif@users.noreply.github.com>
2022-11-27 15:59:30 +0100

    Commented conf part

--- code.py
+++ code.py
@@ -12,23 +12,24 @@ from adafruit_debouncer import Debouncer
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
+#Macropad keycode configuration
 KEYCODES = (
-    [Keycode.ONE],
-    [Keycode.TWO],
-    [Keycode.THREE],
-    [Keycode.FOUR],
-    [Keycode.FIVE],
-    [Keycode.SIX],
-    [Keycode.SEVEN],
-    [Keycode.EIGHT],
-    [Keycode.NINE],
-    [Keycode.ZERO],
-    [Keycode.A],
-    [Keycode.B],
-    [Keycode.C],
-    [Keycode.D],
-    [Keycode.CONTROL, Keycode.C],
-    [Keycode.CONTROL, Keycode.V],
+    [Keycode.ONE], #col 1, row 1, key: '1'
+    [Keycode.TWO], #col 2, row 1, key: '2'
+    [Keycode.THREE], #col 3, row 1, key: '3'
+    [Keycode.FOUR], #col 4, row 1, key: '4'
+    [Keycode.FIVE], #col 1, row 2, key: '5'
+    [Keycode.SIX], #col 2, row 2, key: '6'
+    [Keycode.SEVEN], #col 3, row 2, key: '7'
+    [Keycode.EIGHT], #col 4, row 2, key: '8'
+    [Keycode.NINE], #col 1, row 3, key: '9'
+    [Keycode.ZERO], #col 2, row 3, key: '0'
+    [Keycode.A], #col 3, row 3, key: 'A'
+    [Keycode.B], #col 4, row 3, key: 'B'
+    [Keycode.C], #col 1, row 4, key: 'C'
+    [Keycode.D], #col 2, row 4, key: 'D'
+    [Keycode.CONTROL, Keycode.C], #col 3, row 4, key: 'CTRL'+'C'
+    [Keycode.CONTROL, Keycode.V], #col 4, row 4, key: 'CTRL'+'V'
 )
 
 km = keypad.KeyMatrix(
